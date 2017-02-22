32a7ded075792de0089754c404e6b86c20f13ca7
Nam Tran <namtran0110@gmail.com>
2017-02-22 20:51:32 +0700

    milestone 3 finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,22 @@ class SessionsController < ApplicationController
 
 	  def create
        if @user = User.find_by(email: params[:email])
-         if @user.authenticate(params[:passord]) # check if password is correct
+         if @user.authenticate(params[:password]) # check if password is correct
            	flash[:success] = "signed In Successfully"
            	session[:user_id] = @user.id
-           	redirect_to 'root_path'
+           	redirect_to root_path
          else
            flash.now[:error]= "wrong password!!!"
+           redirect_to root_path
          end
        else
          flash.now[:error] = "user not found"
-   
+         redirect_to root_path
        end
-     end
+    end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
   resources :sessions
+  root "home#index"
+  delete 'log_out' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
